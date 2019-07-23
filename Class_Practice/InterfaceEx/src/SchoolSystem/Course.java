dd65a70cfb4967df7287f79ad492c169432a510b
[Ankit <[ankt84@gmail.com]>
2019-07-22 19:57:23 -0600

    new web project created

--- Class_Practice/InterfaceEx/src/SchoolSystem/Course.java
+++ Class_Practice/InterfaceEx/src/SchoolSystem/Course.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+
 public class Course {
     private final int id;
     private String name;
--- Class_Practice/InterfaceEx/src/SchoolSystem/SchoolMain.java
+++ Class_Practice/InterfaceEx/src/SchoolSystem/SchoolMain.java
@@ -2,6 +2,6 @@ package SchoolSystem;
 
 public class SchoolMain {
     public static void main(String[] args) {
-
+        System.out.println("Hello from the Schoolmain >>>>");
     }
 }
